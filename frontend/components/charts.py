dd5e32698dbfbab39c721aca8c83453b48c9a329
ridhima-2412 <ridhimanagpal2412@gmail.com>
2026-04-13 19:01:38 +0530

    Add distribution plotting function with Streamlit

--- frontend/components/charts.py
+++ frontend/components/charts.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
+import streamlit as st
 
+def plot_distribution(distribution, title):
+    classes = list(distribution.keys())
+    values = list(distribution.values())
+
+    fig, ax = plt.subplots()
+    ax.bar(classes, values)
+    ax.set_title(title)
+    ax.set_xlabel("Class")
+    ax.set_ylabel("Count")
+
+    st.pyplot(fig)
